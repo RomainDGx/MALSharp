badcb03047bcc53884b80cc433297b9106dd236b
RomainDGx <romain.delorme.glorieux@gmail.com>
2025-06-29 19:29:05 +0200

    Create user POCOs

---
+++ MALSharp.Models/User/UserAnimeStatistics.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MALSharp.Models.User;
+
+public class UserAnimeStatistics
+{
+    [JsonPropertyName("num_items_watching")]
+    public int NumItemsWatching { get; set; }
+
+    [JsonPropertyName("num_items_completed")]
+    public int NumItemsCompleted { get; set; }
+
+    [JsonPropertyName("num_items_on_hold")]
+    public int NumItemsOnHold { get; set; }
+
+    [JsonPropertyName("num_items_dropped")]
+    public int NumItemsDropped { get; set; }
+
+    [JsonPropertyName("num_items_plan_to_watch")]
+    public int NumItemsPlanToWatch { get; set; }
+
+    [JsonPropertyName("num_items")]
+    public int NumItems { get; set; }
+
+    [JsonPropertyName("num_days_watched")]
+    public float NumDaysWatched { get; set; }
+
+    [JsonPropertyName("num_days_watching")]
+    public float NumDaysWatching { get; set; }
+
+    [JsonPropertyName("num_days_completed")]
+    public float NumDaysCompleted { get; set; }
+
+    [JsonPropertyName("num_days_on_hold")]
+    public float NumDaysOnHold { get; set; }
+
+    [JsonPropertyName("num_days_dropped")]
+    public float NumDaysDropped { get; set; }
+
+    /// <summary>
+    /// <see cref="NumDaysWatching"/> + <see cref="NumDaysCompleted"/> + <see cref="NumDaysOnHold"/> + <see cref="NumDaysDropped"/>
+    /// </summary>
+    [JsonPropertyName("num_days")]
+    public float NumDays { get; set; }
+
+    [JsonPropertyName("num_episodes")]
+    public int NumEpisodes { get; set; }
+
+    [JsonPropertyName("num_times_rewatched")]
+    public int NumTimesRewatched { get; set; }
+
+    [JsonPropertyName("mean_score")]
+    public float MeanScore { get; set; }
+}
---
+++ MALSharp.Models/User/UserInformation.cs
@@ -0,0 +1,39 @@
+﻿using MALSharp.Models.Converters;
+using System;
+using System.Text.Json.Serialization;
+
+namespace MALSharp.Models.User;
+
+public class UserInformation
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public required string Name { get; set; }
+
+    [JsonPropertyName("picture")]
+    public required Uri Picture { get; set; }
+
+    [JsonPropertyName("gender")]
+    public string? Gender { get; set; }
+
+    [JsonPropertyName("birthday")]
+    public DateOnly? BirthDay { get; set; }
+
+    [JsonPropertyName("location")]
+    public string? Location { get; set; }
+
+    [JsonPropertyName("joined_at")]
+    [JsonConverter(typeof(MALDateTimeConverter))]
+    public DateTime JoinedAt { get; set; }
+
+    [JsonPropertyName("anime_statistics")]
+    public UserAnimeStatistics? AnimeStatistics { get; set; }
+
+    [JsonPropertyName("time_zone")]
+    public string? TimeZone { get; set; }
+
+    [JsonPropertyName("is_supporter")]
+    public bool? IsSupporter { get; set; }
+}
