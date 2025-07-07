ef289a88beca075d9f1fefb6a1e23abb3c629e7b
RomainDGx <romain.delorme.glorieux@gmail.com>
2025-07-07 23:21:51 +0200

    Add missing properties in AuthorWithAvatar

--- MALSharp.Models/Forum/AuthorWithAvatar.cs
+++ MALSharp.Models/Forum/AuthorWithAvatar.cs
@@ -5,6 +5,12 @@ namespace MALSharp.Models.Forum;
 
 public class AuthorWithAvatar : Author
 {
+    [JsonPropertyName("mods_title")]
+    public string? ModsTitle { get; set; }
+
+    [JsonPropertyName("forum_title")]
+    public string? ForumTitle { get; set; }
+
     // Note: typo in field in api V2
     [JsonPropertyName("forum_avator")]
     public required Uri ForumAvatar { get; set; }
