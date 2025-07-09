5580dc290b76ae157b34799abc15f03f086c8eb4
RomainDGx <romain.delorme.glorieux@gmail.com>
2025-07-09 02:44:42 +0200

    Add missing NodePayload POCO

---
+++ MALSharp.Client/Models/NodePayload.cs
@@ -0,0 +1,10 @@
+﻿using MALSharp.Models.Shared;
+using System.Text.Json.Serialization;
+
+namespace MALSharp.Client;
+
+internal class NodePayload<T> where T : Node
+{
+    [JsonPropertyName("node")]
+    public required T Node { get; set; }
+}
