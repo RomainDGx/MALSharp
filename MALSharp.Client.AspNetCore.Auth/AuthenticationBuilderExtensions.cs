bb36cddf776e43cb83fcfe7cdf857a4921bae908
RomainDGx <romain.delorme.glorieux@gmail.com>
2025-08-09 01:49:01 +0200

    Add Authentication extension methods (WIP)

---
+++ MALSharp.Client.AspNetCore.Auth/AuthenticationBuilderExtensions.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MALSharp.Client.AspNetCore.Auth;
+
+public static class AuthenticationBuilderExtensions
+{
+    public static AuthenticationBuilder AddMyAnimeListAuth(this IServiceCollection services, Action<OAuthOptions> configureOptions)
+    {
+        return services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = MALAuthDefaults.AuthenticationScheme;
+        })
+        .AddCookie(options =>
+        {
+            options.Cookie.SameSite = SameSiteMode.None;
+            options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+        })
+        .AddMyAnimeListAuth(configureOptions);
+    }
+
+    public static AuthenticationBuilder AddMyAnimeListAuth(this AuthenticationBuilder builder, Action<OAuthOptions> configureOptions)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(configureOptions);
+
+        return builder.AddOAuth(MALAuthDefaults.AuthenticationScheme, MALAuthDefaults.DisplayName, options =>
+        {
+            options.CallbackPath = "/signin-myanimelist";
+
+            options.AuthorizationEndpoint = MALAuthDefaults.AuthorizationEndpoint;
+            options.TokenEndpoint = MALAuthDefaults.TokenEndpoint;
+            options.UserInformationEndpoint = MALAuthDefaults.UserInformationsEndpoint;
+
+            options.Scope.Add("write:users");
+
+            options.SaveTokens = true;
+            options.UsePkce = true;
+
+            options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+            options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+
+            options.Events = new OAuthEvents
+            {
+                OnRedirectToAuthorizationEndpoint = ctx =>
+                {
+                    var redirectUri = ctx.RedirectUri;
+                    if (ctx.Properties.Items.TryGetValue("code_verifier", out var codeVerifier))
+                    {
+                        redirectUri = QueryHelpers.AddQueryString(redirectUri, new Dictionary<string, string?>
+                        {
+                            ["code_challenge"] = codeVerifier,
+                            ["code_challenge_method"] = "plain" // Use plain method due to MAL OAuth2 implementation.
+                        });
+                    }
+                    ctx.Response.Redirect(redirectUri);
+                    return Task.CompletedTask;
+                },
+                OnCreatingTicket = async ctx =>
+                {
+                    using var req = new HttpRequestMessage(HttpMethod.Get, ctx.Options.UserInformationEndpoint);
+                    req.Headers.Authorization = new AuthenticationHeaderValue("Bearer", ctx.AccessToken);
+
+                    using var res = await ctx.Backchannel.SendAsync(req).ConfigureAwait(false);
+                    res.EnsureSuccessStatusCode();
+
+                    using var stream = await res.Content.ReadAsStreamAsync().ConfigureAwait(false);
+                    var token = await JsonDocument.ParseAsync(stream).ConfigureAwait(false);
+                    ctx.RunClaimActions(token.RootElement);
+                }
+            };
+
+            configureOptions(options);
+        });
+    }
+}
---
+++ MALSharp.Client.AspNetCore.Auth/MALAuthDefaults.cs
@@ -0,0 +1,12 @@
+﻿namespace MALSharp.Client.AspNetCore.Auth;
+
+public class MALAuthDefaults
+{
+    public const string AuthenticationScheme = "MyAnimeList";
+    public const string DisplayName = "MyAnimeList";
+
+    public const string AuthorizationEndpoint = "https://myanimelist.net/v1/oauth2/authorize";
+    public const string TokenEndpoint = "https://myanimelist.net/v1/oauth2/token";
+
+    public const string UserInformationsEndpoint = "https://api.myanimelist.net/v2/users/@me";
+}
